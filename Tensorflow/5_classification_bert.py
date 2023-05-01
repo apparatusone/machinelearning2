10d6591e804a1323d094e90964786577a4f8fb1a
Devin Turner <devinturner@me.com>
2023-05-01 23:39:58 +0200

    add dataset

--- Tensorflow/5_classification_bert.py
+++ Tensorflow/5_classification_bert.py
@@ -11,7 +11,15 @@ import matplotlib.pyplot as plt
 
 tf.get_logger().setLevel('ERROR')
 
-dataset_dir = 'aclImdb'
+url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+
+dataset = tf.keras.utils.get_file("aclImdb_v1", url,
+                                    untar=True, cache_dir='.',
+                                    cache_subdir='')
+
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+
+# dataset_dir = 'aclImdb'
 train_dir = os.path.join(dataset_dir, 'train')
 
 AUTOTUNE = tf.data.AUTOTUNE
